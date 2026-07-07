7a6193b01c3dc8a9b2c2dd99259caaf7b141e806
Nitr0Skay <cin.miko94@gmail.com>
2026-07-07 18:33:22 +0200

    Initial Commit

---
+++ TestProject/Program.cs
@@ -0,0 +1,20 @@
+﻿string stringMessage = "The quick brown fox jumps over the lazy dog.";
+char[] charMessage = stringMessage.ToCharArray();
+
+Array.Reverse(charMessage);
+string newMessage = new String(charMessage);
+
+// Count all the o's
+int o = 0;
+
+foreach (char c in charMessage)
+{
+    if (c == 'o')
+    {
+        o++;
+    }
+}
+
+// Prints the Messages about o number and the Reversed Message
+Console.WriteLine(newMessage);
+Console.WriteLine($"The 'o' character appears {o} times in this message.");
