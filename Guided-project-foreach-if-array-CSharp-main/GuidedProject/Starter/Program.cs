fb5e18538f75bcedf515481c5c3de2af96884214
Nitr0Skay <cin.miko94@gmail.com>
2026-07-10 21:59:53 +0200

    Add student Names array

--- Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
+++ Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
@@ -41,12 +41,14 @@ decimal andrewScore;
 decimal emmaScore;
 decimal loganScore;
 
+
+string[] students = [ "Sophia", "Andrew", "Emma", "Logan" ];
 int[][] gradeBook = [ sophiaGrades, andrewGrades, emmaGrades, loganGrades ];
 
-sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
-andrewSum = andrew1 + andrew2 + andrew3 + andrew4 + andrew5;
-emmaSum = emma1 + emma2 + emma3 + emma4 + emma5;
-loganSum = logan1 + logan2 + logan3 + logan4 + logan5;
+// sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
+// andrewSum = andrew1 + andrew2 + andrew3 + andrew4 + andrew5;
+// emmaSum = emma1 + emma2 + emma3 + emma4 + emma5;
+// loganSum = logan1 + logan2 + logan3 + logan4 + logan5;
 
 sophiaScore = (decimal)sophiaSum / currentAssignments;
 andrewScore = (decimal)andrewSum / currentAssignments;
